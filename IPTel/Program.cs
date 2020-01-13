a9c53350c4f5ce2ea9dfe4abc1d079bc73eadc0b
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-01-13 19:36:41 +0600

    Add project files.

---
+++ IPTel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IPTel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(3231);
+            udpClient.Connect(IPAddress.Parse("10.1.4.82"), 3231);
+            udpClient.Close();
+            UdpClient senderUdp = new UdpClient(3231);
+            var buf = new byte[8192];
+            var bytes = System.Text.Encoding.UTF8.GetString(buf);
+            Console.Beep(buf.Length, 150000);
+            senderUdp.Close();
+        }
+    }
+}
